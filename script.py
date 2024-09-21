72ead4afcc2e4af2c50b5d9ce2194e2b17ca9369
Catarino David Delgado <david@sidewaysthought.net>
2024-09-20 22:30:21 -0500

    Refactor license and update weather code map

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
-# script.py
+"""
+This script is an extension for Text Generation WebUI
+"""
 
 import datetime
 import pytz
@@ -6,46 +8,47 @@ import geocoder
 import requests
 import gradio as gr
 
+# Open Meteo weather code map
 weather_code_map = {
-    0: "Clear sky",
-    1: "Mainly clear",
-    2: "Partly cloudy",
+    0: "Clear",
+    1: "Mostly Clear",
+    2: "Partly Cloudy",
     3: "Overcast",
     45: "Fog",
-    48: "Depositing rime fog",
-    51: "Light drizzle",
-    53: "Moderate drizzle",
-    55: "Dense drizzle",
-    56: "Light freezing drizzle",
-    57: "Dense freezing drizzle",
-    61: "Slight rain",
-    63: "Moderate rain",
-    65: "Heavy rain",
-    66: "Light freezing rain",
-    67: "Heavy freezing rain",
-    71: "Slight snowfall",
-    73: "Moderate snowfall",
-    75: "Heavy snowfall",
-    77: "Snow grains",
-    80: "Slight rain showers",
-    81: "Moderate rain showers",
-    82: "Violent rain showers",
-    85: "Slight snow showers",
-    86: "Heavy snow showers",
-    95: "Thunderstorm: Slight or moderate",
-    96: "Thunderstorm with slight hail",
-    99: "Thunderstorm with heavy hail"
+    48: "Freezing Fog",
+    51: "Light Drizzle",
+    53: "Moderate Drizzle",
+    55: "Dense Drizzle",
+    56: "Light Freezing Drizzle",
+    57: "Dense Freezing Drizzle",
+    61: "Light Rain",
+    63: "Moderate Rain",
+    65: "Heavy Rain",
+    66: "Light Freezing Rain",
+    67: "Heavy Freezing Rain",
+    71: "Light Snow",
+    73: "Moderate Snow",
+    75: "Heavy Snow",
+    77: "Snow Grains",
+    80: "Light Rain Showers",
+    81: "Moderate Rain Showers",
+    82: "Violent Rain Showers",
+    85: "Slight Snow Showers",
+    86: "Heavy Snow Showers",
+    95: "Thunderstorm: Slight or Moderate",
+    96: "Thunderstorm With Light Hail",
+    99: "Thunderstorm With Heavy Hail"
 }
 
 params = {
-    "display_name": "DateTime, Timezone, Location, and Weather Extension",
+    "display_name": "Locality",
     "is_tab": False,
     "add_time": True,
     "add_date": True,
     "add_timezone": True,
     "add_location": True,
     "add_weather": True,
-    "timezone": "UTC"  # Default timezone
+    "timezone": "UTC"
 }
 
 def get_location():
